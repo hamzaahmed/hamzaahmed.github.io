a088a8293b32d27f0fea7b30852b31589792cd2f
Hamza Ahmed <hamza.ah@berkeley.edu>
2015-03-28 18:28:12 -0700

    updated site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,36 @@
 $(document).ready(function() {
 
 
-setTimeout(function() {
-		$('#wrapper').addClass('loaded');
-		$('.navbar-container').addClass('loaded');
+	setTimeout(function() {
+		$('.wrapper').addClass('loaded');
+		$('.nav-container').addClass('loaded');
 	}, 300);
 
-/*
-$(window).scroll(function(event){
-
-   var lastScrollTop = 0;
-   var st = $(this).scrollTop();
-   if (st <= 0) {
-   		$('.navbar-container').addClass('active');
-   } else if (st > lastScrollTop){
-       $('.navbar-container').removeClass('active');
-   } else if  (st < lastScrollTop - 5) {
-       $('.navbar-container').addClass('active');
-   }
-
-   lastScrollTop = st;
-   if (st == 0) {
-   	$('.navbar-container').css('border-bottom', '1px solid transparent');
-   } else if (st - 100 > 0) {
-   		$('.navbar-container').css('border-bottom', '1px solid rgba(0,0,0,0.05)');
-   }
-});*/
-
-	$('#read-button').click(function() {
+	$('.body-text-btn').click(function() {
 		$(this).text(function(i, text) {
           return text === "Read less" ? "Read more" : "Read less";
       	})
 
-		if ($('#more-text').is(":visible")) {
-			$('#more-text').slideUp(300);
+		if ($('.body-text-expand').is(":visible")) {
+			$('.body-text-expand').slideUp(300);
 		} else {
-			$('#more-text').slideDown(300);
+			$('.body-text-expand').slideDown(300);
 		}
 	});
 
 
-	$('.lines-button').click(function() {
+	$('.lines-btn').click(function() {
 		if ($(this).hasClass('close')) {
 			$(this).removeClass('close');
-			$('#navbar-item-container').removeClass('navbar-active');
+			$('.nav-item-container').removeClass('nav-active');
 		} else {
 			$(this).addClass('close');
-			$('#navbar-item-container').addClass('navbar-active');
+			$('.nav-item-container').addClass('nav-active');
 		}
 	});
 
-	$('.navbar-item a').click(function() {
-		$('.lines-button').removeClass('close');
-		$('#navbar-item-container').removeClass('navbar-active');
+	$('.nav-item a').click(function() {
+		$('.lines-btn').removeClass('close');
+		$('.nav-item-container').removeClass('nav-active');
 	});
-
  });
