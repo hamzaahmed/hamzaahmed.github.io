e1cd1da20a165f12647becc95409e343b15af1cc
Hamza Ahmed <hamza.ah@berkeley.edu>
2017-09-17 17:00:29 -0700

    Updating website after moving to SF

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,6 @@
 $(document).ready(function() {
-
-
 	setTimeout(function() {
 		$('.wrapper').addClass('loaded');
 		$('.nav-container').addClass('loaded');
 	}, 300);
-
-	$('.body-text .body-text-btn').click(function() {
-		$(this).text(function(i, text) {
-          return text === "collapse" ? "read more" : "collapse";
-      	})
-
-		if ($('.body-text-expand').is(":visible")) {
-			$('.body-text-expand').slideUp(300);
-		} else {
-			$('.body-text-expand').slideDown(300);
-		}
-	});
-
-
-	$('.lines-btn').click(function() {
-		if ($(this).hasClass('close')) {
-			$(this).removeClass('close');
-			$('.btn-container').removeClass('btn-active');
-			$('.nav-item-container').removeClass('nav-active');
-		} else {
-			$(this).addClass('close');
-			$('.btn-container').addClass('btn-active');
-			$('.nav-item-container').addClass('nav-active');
-		}
-	});
-
-	$('.nav-item a').click(function() {
-		$('.lines-btn').removeClass('close');
-		$('.nav-item-container').removeClass('nav-active');
-		$('.btn-container').removeClass('btn-active');
-	});
  });
